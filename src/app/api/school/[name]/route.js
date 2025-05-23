7ec0f668f584cbace8af615d6a984aa87cd25f30
LSBeryl <dltjgus8098@naver.com>
2025-05-23 09:11:23 +0900

    chore: route.js 주석 추가

--- src/app/api/school/[name]/route.js
+++ src/app/api/school/[name]/route.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export async function GET(_, context) {
+  // API 사용 승인 받으면 쓰고 아니면 지울 파일
   const { params } = await context;
 
   return new Response(JSON.stringify(params), {
